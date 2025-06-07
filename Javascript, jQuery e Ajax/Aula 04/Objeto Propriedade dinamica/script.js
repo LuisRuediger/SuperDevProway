05044958c4642c8fb43c27edfc262b31781e90f5
Luis Ruediger <luisruediger27@gmail.com>
2025-06-06 22:12:58 -0300

    JJA_AULA_04 - Propriedades de objetos dinamicas

---
+++ Javascript, jQuery e Ajax/Aula 04/Objeto Propriedade dinamica/script.js
@@ -0,0 +1,11 @@
+const produto = {};
+
+produto.codigo = 7;
+produto ['nome'] = 'Notebook';
+
+let novaPropriedade = 'status';
+let novoValor = true;
+
+produto[novaPropriedade] = novoValor;
+
+console.log(produto.status);
