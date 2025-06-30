a9a6eb09f825a1bb39aff8f2b21aa248d5992221
Luis Ruediger <luisruediger27@gmail.com>
2025-06-30 20:27:57 -0300

    ANGULAR_AULA_03 - inicio projeto pizzaria

---
+++ Angular/Aula 03/pizzaria/src/app/components/pizza/pizza.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pizza',
+  standalone: false,
+  templateUrl: './pizza.html',
+  styleUrl: './pizza.css'
+})
+export class Pizza {
+
+}
